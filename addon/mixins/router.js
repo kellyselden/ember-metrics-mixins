bada7964ec6ad9c3cd6601ba5509e25a3827528b
Kelly Selden <kellyselden@gmail.com>
2015-10-29 18:39:01 -0400

    adding router mixin

---
+++ addon/mixins/router.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+  didTransition() {
+    this._super(...arguments);
+    this._trackPage();
+  },
+
+  _trackPage() {
+    Ember.run.scheduleOnce('afterRender', () => {
+      this.get('metrics').trackPage({
+        page: document.location.pathname,
+        title: this.get('currentRouteName')
+      });
+    });
+  }
+});
