a27c4c511c61ad8200fd6f0ba50f337b50339744
Kelly Selden <kellyselden@gmail.com>
2017-10-24 10:34:24 -0700

    don't cache the metrics service it will be reused across tests

--- addon/mixins/router.js
+++ addon/mixins/router.js
@@ -3,12 +3,8 @@ import { scheduleOnce } from '@ember/runloop';
 import { assign } from '@ember/polyfills';
 import { getOwner } from '@ember/application';
 
-let metrics;
-
 function trackPage(infos) {
-  if (!metrics) {
-    metrics = getOwner(this).lookup('service:metrics');
-  }
+  let metrics = getOwner(this).lookup('service:metrics');
 
   scheduleOnce('afterRender', () => {
     let options = {
