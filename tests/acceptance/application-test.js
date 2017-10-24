62f100a4732eb67dc2b70310ad1963f29dc43fad
Kelly Selden <kellyselden@gmail.com>
2017-10-24 10:34:24 -0700

    don't cache the metrics service it will be reused across tests

--- tests/acceptance/application-test.js
+++ tests/acceptance/application-test.js
@@ -16,3 +16,25 @@ test('it works between route transitions', function(assert) {
     assert.equal(find('#foo-bar').text().trim(), '/foo/bar foo.bar foo-bar');
   });
 });
+
+let oldMetricsService;
+
+test('it doesn\'t cache the metrics service - part 1 of 2', function(assert) {
+  visit('/');
+
+  let metrics = this.application.__container__.lookup('service:metrics');
+
+  assert.notEqual(metrics, oldMetricsService);
+
+  oldMetricsService = metrics;
+});
+
+test('it doesn\'t cache the metrics service - part 2 of 2', function(assert) {
+  visit('/');
+
+  let metrics = this.application.__container__.lookup('service:metrics');
+
+  assert.notEqual(metrics, oldMetricsService);
+
+  oldMetricsService = metrics;
+});
